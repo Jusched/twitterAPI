d4b81f8ca55e83ffb48c6824b97709cc86bd2dde
Jusched <juans-betancurt@unilibre.edu.co>
2022-09-11 19:28:46 -0500

    models_users

---
+++ models/models.py
@@ -0,0 +1,43 @@
+# Python
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+# Pydantic 
+from pydantic import BaseModel, EmailStr, Field
+
+# FastAPI
+from fastapi import FastAPI
+
+
+# Everything inherits from UserBase because it has the minimum requirements
+# To initialize an user. 
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=30
+    )
+
+# User is used for other things without requiring the password
+class User(BaseModel):
+
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+
+class Tweet(BaseModel):
+    pass
